4c1ee2bff0714d52faa930f57f3037a2fdb0cb02
dotquote <43100990+dotquote@users.noreply.github.com>
2018-09-28 14:59:08 -0400

    Fixed Restart
    -Trimmed the fat off the Restart code and applied it to the Game Situation.
    -Allowed for the Button to push for restart to be changed.

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -1,20 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Restart : MonoBehaviour {
-
-    // Use this for initialization
-    void Start() {
-    }
-
-    // Update is called once per frame
+public class Restart : MonoBehaviour
+{
+    public KeyCode restartKeyCode;
+ 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(restartKeyCode))
         {
-
             SceneManager.LoadScene("Fight");
         }
     }
