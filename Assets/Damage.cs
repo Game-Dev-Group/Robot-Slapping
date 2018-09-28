52bdb6f7b55db0afdb7eb773bfbeda98265adb46
dotquote <43100990+dotquote@users.noreply.github.com>
2018-09-28 07:45:04 -0400

    Health and Damage
    Things Added:
    -Created Health
    -Created Damage
    
    Things that need to be added/changed:
    -Health and damage don't seem to lock movement after 10 hits, which is what the integer is set at now. If someone could examine my code for errors I'd appreciate it.
    -Need to create 2 health slider elements and attach them to the players.

---
+++ Assets/Damage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public int attackDamage = 1;
+    public string opponent;
+
+    Health health;
+    GameObject player;
+    bool InRange;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(opponent);
+        health = player.GetComponent<Health>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+            InRange = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        InRange = false;
+    }
+
+    void Update()
+    {
+        if (InRange)
+            Attack();
+    }
+
+    void Attack()
+    {
+        if (health.CurrentHealth > 0)
+            health.TakeDamage(attackDamage);
+    }
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    public int MaxHealth = 10;
+    public int CurrentHealth;
+
+    Movement movement;
+    HurtBoxMovement hurtBoxMovement;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        movement = GetComponent<Movement>();
+        hurtBoxMovement = GetComponent<HurtBoxMovement>();
+
+        CurrentHealth = MaxHealth;
+    }
+
+    void Update()
+    {
+        damaged = false;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        CurrentHealth -= amount;
+        healthSlider.value = CurrentHealth;
+
+
+        if (CurrentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        movement.enabled = false;
+        hurtBoxMovement.enabled = false;
+    }
+}
