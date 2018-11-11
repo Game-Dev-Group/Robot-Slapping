17c3ccce1be1c096bfdce076fa8021ce8702e023
Owen <ohutchins1@student.gsu.edu>
2018-11-11 15:42:56 -0500

    New Models
    Press Escape to go to Main Menu
    
    Upgraded robot models to a new version that is rigged for animation. (Spent hours trying to do it but got so frustrated I have halted it. If anyone wants to take a shot, be my guest)
    
    Altered the arena a bit.

--- Assets/Restart.cs
+++ Assets/Restart.cs
@@ -4,12 +4,17 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     public KeyCode restartKeyCode;
+    public KeyCode exitMainMenu;
  
     void Update()
     {
         if (Input.GetKeyDown(restartKeyCode))
         {
             SceneManager.LoadScene("Fight");
+        }
+        if (Input.GetKeyDown(exitMainMenu))
+        {
+            SceneManager.LoadScene("Main Menu");
         }
     }
 }
